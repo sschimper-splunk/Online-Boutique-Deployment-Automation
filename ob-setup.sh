137a99cc85cbb0291c392f27bfc25bce267971e8
Sebastian Schimper <sschimper@splunk.com>
2024-09-10 07:58:39 +0100

    trying to fix missing docker.sock file

--- ob-setup.sh
+++ ob-setup.sh
@@ -2,12 +2,11 @@
 
 # install dependencies
 sudo snap install docker
-sudo chmod 777 /var/run/docker.sock
 curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
 sudo install minikube-linux-amd64 /usr/local/bin/minikube && rm minikube-linux-amd64
-
 sudo snap install kubectl --classic
 curl -Lo skaffold https://storage.googleapis.com/skaffold/releases/latest/skaffold-linux-amd64 && sudo install skaffold /usr/local/bin/
+sudo chmod 777 /var/run/docker.sock
 
 # start cluster
 minikube start --cpus=4 --memory 4096 --disk-size 32g --vm-driver=docker
