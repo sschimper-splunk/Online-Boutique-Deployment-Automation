a074c2964a3ee792398d64eb2c319b169459974c
Sebastian Schimper <sschimper@splunk.com>
2025-11-04 09:34:21 +0100

    fixed issue with Docker provided by snap

--- README.md
+++ README.md
@@ -8,6 +8,9 @@ The script will do the following:
 - fetching the Online Boutique source code from GitHub
 - Deploying the Online Boutique via skaffold
 
+> **Warning**
+> Please only use this script in a testing environment and NOT in a production environment!
+
 To set up the enviroment with running Online Boutique, simply follow these steps:
 - Create a Nova/AWS EC2 instance with Ubuntu operating system and instance type at least m5.xlarge (4 cores are needed)
 - SSH into the instance, create an .sh file, copy the contents of the attached script to it, and make it executable with `chmod +x <filename>.sh`
@@ -16,4 +19,4 @@ To set up the enviroment with running Online Boutique, simply follow these steps
 - If you want to access the front-end of the app yourself, on your local machine create a tunnel like so: `ssh -L 8080:localhost:8080 <user>@<remote-server-ip>`, and once connected you can run `kubectl port-forward deployment/frontend 8080:8080`. This makes the frontend available in a browser on localhost:8080 (screenshot of the main page attached).
 
 Now you can try to (auto-)instrumenting the application and try to get some metrics, traces and logs to Splunk Observability Cloud.
-Happy observing! In case you encounter any problems, let me know! 
+Happy observing! In case you encounter any problems, let me know!
--- ob-setup.sh
+++ ob-setup.sh
@@ -1,18 +1,35 @@
 #!/bin/bash
 
-# install dependencies
-sudo snap install docker
-curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
-sudo install minikube-linux-amd64 /usr/local/bin/minikube && rm minikube-linux-amd64
-sudo snap install kubectl --classic
-curl -Lo skaffold https://storage.googleapis.com/skaffold/releases/latest/skaffold-linux-amd64 && sudo install skaffold /usr/local/bin/
+# install docker
+sudo apt-get update
+sudo apt-get install -y ca-certificates curl
+sudo install -m 0755 -d /etc/apt/keyrings
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+  $(. /etc/os-release && echo "${UBUNTU_CODENAME:-$VERSION_CODENAME}") stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 sudo chmod 777 /var/run/docker.sock
 
-# start cluster
+# install minikube
+curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
+sudo install minikube-linux-amd64 /usr/local/bin/minikube && rm minikube-linux-amd64
+eval $(minikube docker-env)
+
+# install kubectl
+sudo snap install kubectl --classic
+
+# install skaffold
+curl -Lo skaffold https://storage.googleapis.com/skaffold/releases/latest/skaffold-linux-amd64 && sudo install skaffold /usr/local/bin/
+
+# start minikube cluster
 minikube start --cpus=4 --memory 4096 --disk-size 32g --vm-driver=docker
 
-# deploy app
+# deploy microservices app
 git clone https://github.com/GoogleCloudPlatform/microservices-demo.git
-cd microservices-demo && skaffold run
+cd microservices-demo && sudo skaffold run
 
 echo "Finished"
