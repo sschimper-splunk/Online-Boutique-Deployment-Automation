4b88273203771903dac5ae8ae3203c7093cf4f19
Sebastian Schimper <sschimper@splunk.com>
2025-11-04 11:00:35 +0100

    changed order of commands

--- ob-setup.sh
+++ ob-setup.sh
@@ -12,12 +12,11 @@ echo \
   sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 sudo apt-get update
 sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
-sudo chmod 777 /var/run/docker.sock
+
 
 # install minikube
 curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
 sudo install minikube-linux-amd64 /usr/local/bin/minikube && rm minikube-linux-amd64
-eval $(minikube docker-env)
 
 # install kubectl
 sudo snap install kubectl --classic
@@ -26,7 +25,9 @@ sudo snap install kubectl --classic
 curl -Lo skaffold https://storage.googleapis.com/skaffold/releases/latest/skaffold-linux-amd64 && sudo install skaffold /usr/local/bin/
 
 # start minikube cluster
+sudo chmod 777 /var/run/docker.sock
 minikube start --cpus=4 --memory 4096 --disk-size 32g --vm-driver=docker
+eval $(minikube docker-env)
 
 # deploy microservices app
 git clone https://github.com/GoogleCloudPlatform/microservices-demo.git
